b225a01fd86d03d4d7a36717f99294afe43a2415
sravan reddy <sravanreddy234@gmail.com>
2017-07-27 21:29:02 +0530

    Project Completed (Session maintained)

--- src/com/moreProject/cartServlets/DeleteCartServlet.java
+++ src/com/moreProject/cartServlets/DeleteCartServlet.java
@@ -1,20 +1,35 @@
 package com.moreProject.cartServlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.moreProject.db.CartDAO;
+import com.moreProject.db.CrudDAO;
 
 @WebServlet("/DeleteCartServlet")
 public class DeleteCartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  
+        
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession(false);
+		String pid= request.getParameter("ProductId");
+		String uid =(String) session.getAttribute("UserId");
+		
+		
+        CartDAO.delete(pid, uid);
+        response.sendRedirect("ViewCartServlet");  
+    }  
 
 }
